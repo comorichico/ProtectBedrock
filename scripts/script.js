ca38b43dde0090378f45f65dd10b67f3fdd90ff4
comorichico <chico@comorichico.com>
2023-02-04 07:38:26 +0900

    blockBreakとblockPlaceの動作確認

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,9 @@ world.events.blockBreak.subscribe(ev => {
   const dimension = ev.dimension;
   dimension.runCommandAsync(`say ${ev.player.name}が${id}を壊しました`);
 });
+
+world.events.blockPlace.subscribe(ev => {
+  const id = ev.block.type.id;
+  const dimension = ev.dimension;
+  dimension.runCommandAsync(`say ${ev.player.name}が${id}を置きました`);
+});
